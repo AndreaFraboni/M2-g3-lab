a4f90c6c9c92dc0532ea9ae85384255f8aaac9a3
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2025-10-21 18:40:52 +0100

    Exercize 3 ADDED

---
+++ Assets/_Project/EX3/Scripts/EX3.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EX3 : MonoBehaviour
+{
+
+    [SerializeField] int Numero;
+
+    int restodivisione;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        restodivisione = Numero % 2;
+
+        if (restodivisione ==0)
+        {
+            Debug.Log("Il numero = " + Numero + " è PARI !!!!");
+        }
+        else
+        {
+            Debug.Log("Il numero= " + Numero + " è DISPARI !!!");
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
